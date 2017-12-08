5ab88bb1bc9159eb9201aaf3a84f5708aadee9b1
Eva <evaliisagalen@gmail.com>
2017-12-08 10:30:44 -0800

    created ajax function and set all received data into variables

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,49 @@
 // define globals
 var weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
+// lists quakes mag 4.5+ for the past week
 
 $(document).ready(function() {
   console.log("Let's get coding!");
   // CODE IN HERE!
+
+      let ajaxOptions = {
+        method: 'GET',
+        url: weekly_quakes_endpoint,
+        dataType: 'json',
+        success: onSuccess,
+        error: onError
+      };
+
+      $.ajax(ajaxOptions);
+
+      function onSuccess(quakeData) {
+        //debugger;  use to 'pause' website from executing code that follows. It allows to play with and acces the fetched data (e.g. to find paths)
+        // console.log(quakeData)
+        let quakes = quakeData.features; //creates an array of the data
+        //console.log(quakes);
+        let quakeTitles = quakes.map (function(quake) {return  quake.properties.title}); // gets all quake titles
+        //console.log(quakeTitles);
+        let quakeTime = quakes.map (function(quake) {return  quake.properties.time}); // gets all quake times
+        //let timeSince = Math.round((Date.now() - quakeTime)/ 60 / 60 / 1000);
+
+        //console.log(quakeTime);
+        let quakeCoordinates = quakes.map (function(quake) {return  quake.geometry.coordinates});
+        console.log(quakeCoordinates);
+        quakeTitles.map (function(title){  //map function appends mutiple images in a loop
+          $("div#info").append(`<p>${title}</p>`);
+        });
+     };
+
+      function onError() {
+        console.log("error");
+      };
+
+      $.ajax(ajaxOptions);
+
+      // setTimeout(function() {
+      //   $.ajaxOptions(ajaxOptions);
+      // }, //specify time);
+
+
 
 });
